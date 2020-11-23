933d6ba0eeb6aee029ee720acae82d2b6328ff60
Amit Kumar <amkumar@mdsol.com>
2020-11-23 15:25:19 +0530

    Add callback to add initals to doctors name

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,4 +3,12 @@ class Doctor < ApplicationRecord
   validates :name, :contact_no, :gender, presence: true
   validates :contact_no, length: { is: 10 }
   validates :contact_no, format: { with: /\A[7-9][0-9]{9}\z/, message: 'Contact no should start from 7-9' }
+
+  before_save :add_initals
+
+  private
+
+  def add_initals
+    self.name = 'Dr. ' + self.name unless self.name =~ /\ADr\.\s.*/
+  end
 end
