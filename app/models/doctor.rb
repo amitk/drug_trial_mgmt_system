759ed4ffe9e96c2a161c71a1e6f4b3fbd74009f6
Amit Kumar <amkumar@mdsol.com>
2020-11-24 12:20:27 +0530

    Establish many to many relations between clinics and doctors
      + create a model clinic
      + create a migration for clinic
      + create migration for clinics_doctors
      + add crub for clinic

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,7 @@
 class Doctor < ApplicationRecord
 
+  has_and_belongs_to_many :clinics
+
   validates :name, :contact_no, :gender, presence: true
   validates :contact_no, length: { is: 10 }
   validates :contact_no, format: { with: /\A[7-9][0-9]{9}\z/, message: 'Contact no should start from 7-9' }
