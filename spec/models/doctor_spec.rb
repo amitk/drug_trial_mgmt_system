69bd7a1159b4a136fe9293397377308a346f555c
Amit Kumar <amkumar@mdsol.com>
2021-09-30 17:42:29 +0530

    Add and step rspec-rails

---
+++ spec/models/doctor_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Doctor do
+
+  describe '#validations' do
+
+    context 'when name is absent' do
+      it 'fails on creating doctor record' do
+      end
+    end
+
+    context 'when name is present' do
+      it 'creates doctor record' do
+      end
+    end
+  end
+end
